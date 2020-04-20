2fe1cf6f3f4e63cfdffe154ae9d7df61c281298b
Filip Gabrys <el.filberto@gmail.com>
2020-04-20 19:59:46 +0200

    Add values to sales dict

--- main.py
+++ main.py
@@ -33,6 +33,7 @@ sales2016 = [int(i.replace(",", "")) for i in sales2016]
 sales2017 = [int(i.replace(",", "")) for i in sales2017]
 sales2018 = [int(i.replace(",", "")) for i in sales2018]
 #Stworzenie pod-słownika
-sales= {"2016":[],"2017":[],"2018":[]}
+sales= {"2016":[sales2016],"2017":[sales2017],"2018":[sales2018]}
+
 
 
